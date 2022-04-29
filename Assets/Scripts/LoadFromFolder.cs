752f4df6808955ec94f289e5a5cde421d96d9ad5
superanretan <superanretanek@gmail.com>
2022-04-30 01:48:11 +0200

    fix

--- Assets/Scripts/LoadFromFolder.cs
+++ Assets/Scripts/LoadFromFolder.cs
@@ -17,11 +17,7 @@ public static class LoadFromFolder
     public static Texture2D LoadImageToTexture(string path)
     {
         WWW imageLoad = new WWW(path);
-<<<<<<< HEAD
         Texture2D imageTexture = new Texture2D(imageLoad.texture.width,imageLoad.texture.height); 
-=======
-        Texture2D imageTexture = new Texture2D(2048, 2048); 
->>>>>>> ea5e2828c6622b7b2c886be18a0345180af19765
         imageLoad.LoadImageIntoTexture(imageTexture);
         return imageTexture;
     }
@@ -36,6 +32,5 @@ public static class LoadFromFolder
         var timeDif = DateTime.Now - Directory.GetCreationTime(path);
         return timeDif.Days + " Days " + timeDif.Hours + " Hours " + timeDif.Minutes + " Minutes ";
     }
-    
     
 }
